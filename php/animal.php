9fbe30f5311f34cfadea0907b607820cb9ee8913
Candelier.M <58921922+ravenqueenofinsanity@users.noreply.github.com>
2024-02-21 10:35:10 +0100

    add logic for the animal view

--- php/animal.php
+++ php/animal.php
@@ -1,51 +1,108 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Animaux</title>
-</head>
-<body>
-   
-    <h1>Animaux</h1>
-   <!--show only the validate habitat-->
-   <?php
-    require('connexion.php');
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    // display only the validate comments
-    $query = "SELECT * FROM animaux";
-    $result = mysqli_query($conn, $query);
-    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    ?>
-        
-        <?php foreach ($row as $row): ?>
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title"><?php echo $row['prenom']; ?></h5>
-    <p class="card-text"><?php echo $row['race']; ?></p>
-    <p class="card-text"><small class="text-muted"><?php echo $row['habitat']; ?></small></p>
-    <a href="Lion_sante.php" class="btn btn-primary fiche">Carnet de santé</a>
-  </div>
-  <img class="card-img-bottom" src="../images/lion1.jpg" alt="Card image cap">
-</div>
-<?php endforeach; ?>
-    
-<!--if link from the card is click add 1 to view in the animaux table?-->
-        <?php
-        if(){
-            $query = "UPDATE animaux SET view = view + 1 WHERE id = 1";
-            mysqli_query($conn, $query);
 
-        }
-   
-        ?>
-        <br>
-    
-        <button onclick="history.back()">Go Back</button>
-        <a href="admin.php">Home</a>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Animaux</title>
+</head>
+
+<body>
+  <?php
+  session_start();
+  require('connexion.php');
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $query = "SELECT * FROM animaux";
+  $result = mysqli_query($conn, $query);
+
+  if ($result) {
+    $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  } else {
+    echo "Error: " . mysqli_error($conn) . $row;
+  }
+  //si bouton view click envoyer on incremente de 1 la colonne view
+  if (isset($_POST['view'])) {
+    $view = $_POST['view'];
+    $query = "UPDATE animaux SET view = view + 1 WHERE view = '$view'";
+    $result = mysqli_query($conn, $query);
+    if ($result) {
+      //header('location: animal.php');
+      echo 'View incremented successfully';
+
+    } else {
+      echo "Error: " . mysqli_error($conn);
+    }
+  }
+ // si le bouton animal détail est clické, envoyer vers la page qui correspond à l'animal selectionné
+  if (isset($_POST['animal'])) {
+    $animal = $_POST['race'];
+    if ($animal == 'Lion') {
+      header('location: Lion_sante.php');
+    } elseif
+    ($animal == 'zebre') {
+      header('location: zebre_sante.php');
+    } elseif
+    ($animal == 'girafe') {
+      header('location: girafe_sante.php');
+    } elseif
+    ($animal == 'Chimpanze') {
+      header('location: chimpanze_sante.php');
+    } elseif
+    ($animal == 'aigle') {
+      header('location: aigle_sante.php');
+    }
+  }
+  ?>
+ 
+     
+  <h1>Animaux</h1>
+
+  <!-- Display all animals -->
+  <div class="table-responsive">
+    <table class="table table-bordered">
+      <tr>
+        <th>Id</th>
+        <th>Prenom</th>
+        <th>Habitat</th>
+        <th>Race</th>
+        <th>View</th>
+      </tr>
+      <?php foreach ($row as $animal) : ?>
+        <tr>
+          <td><?php echo  $animal['id'] ?></td>
+          <td><?php echo $animal['prenom']; ?></td>
+          <td><?php echo $animal['habitat']; ?></td>
+          <td><?php echo $animal['race']; ?></td>
+          <td><?php echo $animal['view']; ?></td>
+          <td>
+            <form action="animal.php" method="post">
+              <input type="hidden" name="view" value="<?php echo $animal['view']; ?>">
+              <input type="submit" value="View">
+              <!--link qui envoie vers les détails de l'naimal-->
+              <a href="animal_detail.php?id=<?php echo $animal['id']; ?>">Details</a>
+          <!--si race = lion envoyer vers la page lion_sante.php-->
+              <input type="hidden" name="id" value="<?php echo $animal['id']; ?>"/>
+              <button type="button" onclick="showSanté(this)" class="btn btn-primary">Santé</button>
+            </form>
+          </td>
+        </tr>
+      <?php endforeach; ?>
+    </table>
+  </div>
+
+
+  <?php if (empty($animal)): ?>
+        <p>No animals found.</p>
+    <?php endif; ?>
+
+  <button onclick="history.back()">Go Back</button>
+  <a href="admin.php">Home</a>
 
 </body>
+
 </html>
